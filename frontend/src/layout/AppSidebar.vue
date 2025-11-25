a3bfc98ca013aeccb8176f8c9fc4aa61d8d15357
isallmuhammad42 <isallmuhammad42@gmail.com>
2025-11-26 03:12:30 +0700

    first commit

---
+++ frontend/src/layout/AppSidebar.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div class="layout-sidebar">
+        <!-- <app-menu></app-menu> -->
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div class="card">
+        <h1 class="text-3xl font-bold mb-4">Dashboard</h1>
+        <p>Welcome to your dashboard</p>
+    </div>
+</template>
